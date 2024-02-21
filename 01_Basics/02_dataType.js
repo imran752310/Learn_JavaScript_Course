e90dd5bdc5cc90ab7a5999fb07625ac851891ea5
Muhammad imran khan <81646567+imran752310@users.noreply.github.com>
2024-02-21 06:02:14 +0000

    datatype

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,20 @@
+"Use Strict"//treat all js Code as a New Version
+//tc39 amd mpm see about variable and style of writing code
+
+let name="imran";
+let age =23;
+let isLogin =false;
+let state;
+
+//number 
+//bigint
+//string =""
+//boolean = true/false
+//null = standalon value //null type is Object 
+//undefine
+//symbol  = is used in react for unique component 
+
+//object
+
+console.log(typeof null)// object
+console.log(typeof undefined);
