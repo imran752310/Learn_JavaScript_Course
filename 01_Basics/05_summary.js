ab139bb53e35f0c846239d55eb34b5e4cb2c20bd
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-23 15:57:44 +0500

    summary basic

---
+++ 01_Basics/05_summary.js
@@ -0,0 +1,30 @@
+//Primitive
+
+// 7 type : String, Number, Boolean, Null, undefine symbol, Bigint
+
+const scoure= 100;
+
+const isLoggedIn = false;
+const number = null;
+const ali = undefined;
+
+const id = Symbol("1234");
+const Bigint    = 987654321234;
+
+
+
+// Non Primitive 
+
+// Array, Objects, Function
+
+let Names = ["Ali", "Hassan","wali","Aslam"];
+
+let car ={
+    color:"red",
+    model: "BMW",
+    height: 1.5,
+}
+
+const Add =  function() {
+    return(a+b);
+}
