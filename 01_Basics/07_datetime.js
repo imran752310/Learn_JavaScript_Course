5042f2305a92bb770a73cf709b7239aacb1ce43a
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-24 17:18:41 +0500

    ok

---
+++ 01_Basics/07_datetime.js
@@ -0,0 +1,13 @@
+//Date
+
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString())
+
+console.log(typeof myDate);
+
+let timeStamp = Date.now();
+console.log(timeStamp);
