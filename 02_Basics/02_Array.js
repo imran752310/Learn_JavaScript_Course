deefca2a249fc45c4f87b580406f30d8aa046ae1
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-02-27 19:25:33 +0500

    OK

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,7 @@
+const Array1 = ["imran", "khan", "Aslam", "Ali"];
+const Array2 = ["wajid", "fasial", "sahil", "hamza"];
+
+Array1.push(Array2); // adding array 2 to array
+// console.log(Array1);
+
+console.log(Array1[1][2]); // length of the combined
