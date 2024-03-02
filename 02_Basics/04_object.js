534efa31f252ecad80b70df7fab45164815cea55
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-03-02 11:59:53 +0500

    ok

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -38,12 +38,12 @@ const obj2 ={3 : "c", 4 : "d"}
 //const obj3 = {obj1 , obj2};
 //console.log(obj3);//output { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
 const obj3 = Object.assign({}, obj1, obj2)// the {} is source add  to which object we want to join.
-console.log(obj3);
+// console.log(obj3);
 // we not use the above
 
 //We Use Spreed Operator for joining Object
 const obj4 = {...obj1 , ...obj2} // this is also way of joining two objects
-console.log(obj4);
+// console.log(obj4);
 
 //
 
@@ -68,15 +68,35 @@ User1[1].email;
 // let userEmail= User1.find((user)=>{return user.id == 2})
 // console.log(userEmail.email);
 
-console.log(nonSingalTnObjt);
+// console.log(nonSingalTnObjt);
 
-console.log(Object.keys(nonSingalTnObjt));//prnt Keys
-console.log(Object.values(nonSingalTnObjt));// print Values
+// console.log(Object.keys(nonSingalTnObjt));//prnt Keys
+// console.log(Object.values(nonSingalTnObjt));// print Values
 
-console.log(Object.entries(nonSingalTnObjt));// print [ [ 'id', '123' ], [ 'name', 'Imran Khan' ], [ 'status', false ] ]
+// console.log(Object.entries(nonSingalTnObjt));// print [ [ 'id', '123' ], [ 'name', 'Imran Khan' ], [ 'status', false ] ]
 
-console.log(Object.hasOwnProperty(nonSingalTnObjt));//prnt Keys
+// console.log(Object.hasOwnProperty(nonSingalTnObjt));//prnt Keys
 
 
+const course ={
+
+    course : "JavaScript",
+    price : 234,
+    instructor : "Imran Khan"
+}
+
+// Object Destrucrureing
+//course.instructor //this method is also good for single valu print
+
+const {instructor: abc} = course
+
+console.log(abc);
+
+//JSON OBJECt Not real Object
+// {
+//     "Name" : "Imarn",
+//     "Coursename" : "JavaScript",
+//     "Price" : 2344
+// }
 
 
