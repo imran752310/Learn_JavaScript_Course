e29fe3ca16daec3ada2d0534e48f666e58f38d43
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-03-02 12:47:31 +0500

    ok

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,18 @@
+// function addNumber(num1 ,num2){//num1 and num2 are Parameters
+//     console.log( num1 + num2);
+// }
+
+// addNumber(5, 7); // returns 12  // 5 and 7 is Aurgument
+// addNumber(5, '7'); // returns 47
+// addNumber(5, null); // returns 5
+// addNumber(5); // returns NaN
+// // if stor the afunction variable and print
+// const result = addNumber(2, 4);
+//console.log(result);//print undefine it mean function not exectue
+
+function addNumber(num1 ,num2){//num1 and num2 are Parameters
+    return num1 + num2;
+}
+
+const sum = addNumber(3, 9)
+console.log("Result : ", sum); //returns 12
