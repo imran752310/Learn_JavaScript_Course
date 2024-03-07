56166a64a72b1d30f0be35d7e07e051affcd9f12
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-03-07 09:55:50 +0500

    ok

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,35 @@
+//Immediate Invoked Function Experession (IIFE)
+
+// function Connect(){
+//     console.log("DB CONNECTED");
+// }
+
+// Connect()//this create error in Global Scope palution remove by Private fnction
+
+//but now i Use IIFE
+
+//syntex ()()
+
+//named IIFE
+(
+    function(){
+        var db = "Connected to DB";
+        console.log(db);
+        
+        //accessing the variable outside of this scope will give an error because it is private to this function
+        //accessing the variable outside of this scope will give an error
+
+    })();
+
+    //SImple IIFE
+
+( () => {
+        console.log(`BD is Connected`);
+})()
+
+// how to pass argument to IIFE function
+
+
+((name) => {
+    console.log(`Hi ${name} , the DB is CONNECTED`)
+})('Imran Khan')
