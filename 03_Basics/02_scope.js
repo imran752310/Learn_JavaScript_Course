830f803f03023256f7e1ceed83ab3d66b9225cf2
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-03-07 09:08:02 +0500

    ok

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,42 @@
+//global scope
+var c = 200;
+
+let a = 3000;
+//scop is in {} bracket
+
+if(true){
+    //inside {} braket called block scope
+    let a = 10;
+    const b = 30;
+    var c = 39;
+}
+
+// console.log(a)
+// //console.log(b)
+// console.log(c)
+
+
+// another lect for scope and closer
+
+function one(){
+    const name = "imran";
+    function two(){
+        const website = "facebook"
+        console.log(name);
+    }
+   // console.log(website);//error the two function not execute
+    two();
+}
+
+one()
+
+
+if(true){
+    const username = "imran";
+    if(username ==="imran"){
+        const website = "Facebook";
+        console.log(`${username} uses ${website}`);
+    }
+  //  console.log(website);// out of scope
+}
+// console.log(username);//out of scope
