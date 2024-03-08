9ff36a9a382b4ac9f9b9ed9fc85740465933acc5
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-03-08 12:20:07 +0500

    ok

--- 05_Iteration/05_foreach.js
+++ 05_Iteration/05_foreach.js
@@ -8,8 +8,46 @@ coding.forEach( function (val) {
 
 //usring Arrow function or call back function
 
+coding.forEach( (val) =>{
+  //  console.log(val + ' is a popular language');
+})
+
+//using function 
+
+function printMe(val){
+    //console.log(val);
+}
+
+coding.forEach(printMe);
 
 
+// to Print more argument
+
+coding.forEach( (item, index, arr) =>{
+    //console.log(item , index, arr);
+})
+
+// desturtureing Arry having Multiple Objects
+
+const myCode = [
+    {
+        bookName : "js",
+        autherName : "Ali",
+    },
+    {
+        bookName : "React",
+        autherName : "wali",
+    },
+    {
+        bookName : "node",
+        autherName : "majeed",
+    }
+]
+
+myCode.forEach( (item) =>{
+   // console.log(item);// access the Array
+    console.log(item.bookName , item.autherName);// access the Array and also object property
+})
 
 
 
