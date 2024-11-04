492de0f2f0d788a0d8bccd056a23853551749898
Muhammad imran khan <muhammadimran752310@gmail.com>
2024-11-04 17:21:06 +0500

    ok

--- 05_Iteration/08_reduce.js
+++ 05_Iteration/08_reduce.js
@@ -29,6 +29,13 @@ const shoppingCart = [
     },
 ]
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
-console.log(priceToPay);
+// console.log(priceToPay);
+
+shoppingCart.map((item)=>(
+    console.log(item.itemName + " =  " + item.price)
+))
+
+
+
